7095709c9b52f32f85f1be85507e6013fe3036ad
Juliyat Yeldho <juliyat_yeldho@comcast.com>
2021-06-12 23:09:25 -0700

    commiting to child branch

--- src/com/company/Validparantesis.java
+++ src/com/company/Validparantesis.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Validparantesis {
     public static void main(String[] args) {
-        System.out.println("Check in ");
+        System.out.println("Checkin to test branch");
     }
 }
